b81b5f15b4a78fc13a73a049ac1342da0ef54745
budi1200 <alenab22@gmail.com>
2018-10-04 21:06:11 +0200

    work in progress message

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,8 @@ class Header extends Component{
             <div className="header-wrapper">
                 <img height="40" src='./assets/img/logo.svg'/>
 
+                <span style={{color: "white", fontSize: "1.2rem", fontWeight: "bold"}}>! Work In Progress !</span>
+
                 <div>
                     <ul>
                         <li>Domov</li>
