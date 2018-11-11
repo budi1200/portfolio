a07c9679ac7de6988e5ca2ed241863edda649027
budi1200 <alenab22@gmail.com>
2018-11-11 14:45:06 +0100

    update header style

--- src/projects.js
+++ src/projects.js
@@ -1,53 +1,56 @@
-const projects ={"projects": [
-    {
-        "id": 1,
-        "name": "Matematični Pojmi",
-        "img": "./assets/img/project_imgs/matematicni_pojmi.png",
-        "framework": "JavaScript (Canvas)",
-        "lang": "JavaScript"
-    },
-    {
-        "id": 2,
-        "name": "VR-Trener",
-        "img": "./assets/img/project_imgs/vr-trener.jpg",
-        "framework": "Javascript (A-Frame)",
-        "lang": "JavaScript"
-    },
-    {
-        "id": 3,
-        "name": "Prevoz",
-        "img": "./assets/img/project_imgs/prevoz.png",
-        "framework": "Ruby On Rails",
-        "lang": "Ruby"
-    },
-    {
-        "id": 4,
-        "name": "SAŠA Inkubator",
-        "img": "./assets/img/project_imgs/sasa-inkubator.jpg",
-        "framework": "React Native",
-        "lang": "JavaScript"
-    },
-    {
-        "id": 5,
-        "name": "Faucon Wines",
-        "img": "./assets/img/project_imgs/fauconwines.png",
-        "framework": "ReactJS",
-        "lang": "JavaScript"
-    },
-    {
-        "id": 6,
-        "name": "Magnum Logistika",
-        "img": "./assets/img/project_imgs/magnumlogistika.jpg",
-        "framework": "ReactJS",
-        "lang": "JavaScript"
-    },
-    {
-        "id": 8,
-        "name": "Future40",
-        "img": "./assets/img/project_imgs/future40.png",
-        "framework": "ReactJS",
-        "lang": "JavaScript"
-    }
-]}
+const projects ={
+    "projects": 
+    [
+        {
+            "id": 1,
+            "name": "Matematični Pojmi",
+            "img": "./assets/img/project_imgs/matematicni_pojmi.png",
+            "framework": "JavaScript (Canvas)",
+            "lang": "JavaScript"
+        },
+        {
+            "id": 2,
+            "name": "VR-Trener",
+            "img": "./assets/img/project_imgs/vr-trener.jpg",
+            "framework": "Javascript (A-Frame)",
+            "lang": "JavaScript"
+        },
+        {
+            "id": 3,
+            "name": "Prevoz",
+            "img": "./assets/img/project_imgs/prevoz.png",
+            "framework": "Ruby On Rails",
+            "lang": "Ruby"
+        },
+        {
+            "id": 4,
+            "name": "SAŠA Inkubator",
+            "img": "./assets/img/project_imgs/sasa-inkubator.jpg",
+            "framework": "React Native",
+            "lang": "JavaScript"
+        },
+        {
+            "id": 5,
+            "name": "Faucon Wines",
+            "img": "./assets/img/project_imgs/fauconwines.png",
+            "framework": "ReactJS",
+            "lang": "JavaScript"
+        },
+        {
+            "id": 6,
+            "name": "Magnum Logistika",
+            "img": "./assets/img/project_imgs/magnumlogistika.jpg",
+            "framework": "ReactJS",
+            "lang": "JavaScript"
+        },
+        {
+            "id": 8,
+            "name": "Future40",
+            "img": "./assets/img/project_imgs/future40.png",
+            "framework": "ReactJS",
+            "lang": "JavaScript"
+        }
+    ]
+}
 
 export {projects}
